dba580221d53a086faedef3147ee997fbf76606d
Kaoutar EL AZZAB <51494218+kaoutarell@users.noreply.github.com>
2025-01-03 02:10:48 +0100

    Updated Contact.js (backend)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,7 +38,7 @@ export const Contact = () => {
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
-    if (result.code == 200) {
+    if (result.code != 500) {
       setStatus({ succes: true, message: "Message sent successfully" });
     } else {
       setStatus({
