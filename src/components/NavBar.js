7bfa1d3a323dcbf7242d713088f808db5b8581b7
Kaoutar EL AZZAB <51494218+kaoutarell@users.noreply.github.com>
2025-02-04 19:02:36 -0500

    Update NavBar.js - remove let's connect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -83,11 +83,6 @@ export const NavBar = () => {
                   <img src={navIcon3} alt="" />
                 </a>
               </div>
-              <HashLink to="#connect">
-                <button className="vvd">
-                  <span>Let’s Connect</span>
-                </button>
-              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
