1b65aaa80c2485a4d93a91aa1855c589652f3c3f
Kaoutar EL AZZAB <51494218+kaoutarell@users.noreply.github.com>
2025-02-04 19:00:53 -0500

    Update Projects.js - Animation removed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,13 +12,10 @@ import react_ from "../assets/img/react.png";
 import python from "../assets/img/python.png";
 import az900 from "../assets/img/az900.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import TrackVisibility from "react-on-screen";
 import { Technology } from "./Technologies";
 import { Certification } from "./Certification";
 
 export const Projects = () => {
-  const [hasAnimated, setHasAnimated] = useState(false); // Tracks animation state
-
   const projects = [
     {
       title: "Car Rental App",
@@ -56,78 +53,56 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) => {
-                if (isVisible && !hasAnimated) {
-                  setHasAnimated(true); // Trigger the animation once
-                }
-
-                return (
-                  <div
-                    className={`${
-                      hasAnimated ? "slide-in" : "hidden"
-                    } transition-all duration-300 ease-in-out`}
-                  >
-                    <h2>Projects</h2>
-                    <p>
-                      ✨ As a software engineering student, I have worked on
-                      diverse projects, including designing database systems,
-                      developing game engines in C++, and implementing web app
-                      solutions. I have also earned certifications in Azure
-                      Fundamentals and am aiming to obtain additional Microsoft
-                      technology certifications ✨
-                    </p>
-                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                      <Nav
-                        variant="pills"
-                        className="nav-pills mb-5 justify-content-center align-items-center"
-                        id="pills-tab"
-                      >
-                        <Nav.Item>
-                          <Nav.Link eventKey="first">School Projects</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                          <Nav.Link eventKey="second">
-                            Technologies Used
-                          </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                          <Nav.Link eventKey="third">Certifications</Nav.Link>
-                        </Nav.Item>
-                      </Nav>
-                      <Tab.Content
-                        id="slideInUp"
-                        className={`${
-                          hasAnimated ? "slide-in-up" : "hidden"
-                        } transition-all duration-300 ease-in-out`}
-                      >
-                        <Tab.Pane eventKey="first">
-                          <Row>
-                            {projects.map((project, index) => (
-                              <ProjectCard key={index} {...project} />
-                            ))}
-                          </Row>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="second">
-                          <Row className="mb-4 g-3">
-                            {technologies.map((tech, index) => (
-                              <Technology key={index} {...tech} />
-                            ))}
-                          </Row>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="third">
-                          <Row className="d-flex justify-content-center align-items-center">
-                            {certifications.map((cert, index) => (
-                              <Certification key={index} {...cert} />
-                            ))}
-                          </Row>
-                        </Tab.Pane>
-                      </Tab.Content>
-                    </Tab.Container>
-                  </div>
-                );
-              }}
-            </TrackVisibility>
+            <div>
+              <h2>Projects</h2>
+              <p>
+                ✨ As a software engineering student, I have worked on diverse
+                projects, including designing database systems, developing game
+                engines in C++, and implementing web app solutions. I have also
+                earned certifications in Azure Fundamentals and am aiming to
+                obtain additional Microsoft technology certifications ✨
+              </p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav
+                  variant="pills"
+                  className="nav-pills mb-5 justify-content-center align-items-center"
+                  id="pills-tab"
+                >
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">School Projects</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Technologies Used</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Certifications</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Tab.Content id="slideInUp">
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {projects.map((project, index) => (
+                        <ProjectCard key={index} {...project} />
+                      ))}
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">
+                    <Row className="mb-4 g-3">
+                      {technologies.map((tech, index) => (
+                        <Technology key={index} {...tech} />
+                      ))}
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <Row className="d-flex justify-content-center align-items-center">
+                      {certifications.map((cert, index) => (
+                        <Certification key={index} {...cert} />
+                      ))}
+                    </Row>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
